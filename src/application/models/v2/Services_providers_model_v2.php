8ef1b04cae0686ed406bfc80f60ee8c602575127
canhchi <canhlc@davido.vn>
2018-12-05 18:30:30 +0700

    remaned provider model

---
+++ src/application/models/v2/Services_providers_model_v2.php
@@ -0,0 +1,46 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      Davido Team
+ * @copyright   Copyright (c) 2013 - 2018, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Services Providers Model v2
+ *
+ * @package Models
+ */
+class Services_Providers_Model_V2 extends CI_Model {
+
+    /**
+     * Find service by id_integrated
+     * @param $serviceId
+     * @return string
+     * @throws Exception
+     */
+    public function get_providers_by_service_id($serviceId)
+    {
+        if ( ! isset($serviceId))
+        {
+            throw new Exception('$serviceId is not provided: ' . print_r($serviceId, TRUE));
+        }
+
+        $query = $this->db->get_where('ea_services_providers', ['id_services' => $serviceId ]);
+
+        if ($query->num_rows() == 0)
+        { // Check if id_integrated exists in ea_$services
+            throw new \EA\Engine\Api\V1\Exception('$serviceId does not exist in DB: ' . $serviceId, 404, 'Not Found');
+        }
+
+        $services_providers = $query->num_rows() > 0 ? $query->result_array() : '';
+
+        return $services_providers;
+    }
+
+}
