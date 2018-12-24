74c92b311f48be07437e601fff4c0f0ac244eab7
jasminecrazy <suongpham53@gmail.com>
2018-12-24 22:59:07 +0700

    change provider request response

--- src/engine/Api/V2/Parsers/ProvidersV2.php
+++ src/engine/Api/V2/Parsers/ProvidersV2.php
@@ -5,10 +5,11 @@ class ProvidersV2 extends Providers {
 	public function encode(array &$response)
     {
         $id_integrated = $response['id_integrated'];
-        
+        $photo_profile = $response['photo_profile'];
         parent::encode($response);
         
         $response['id_integrated'] = $id_integrated;
+        $response['photo_profile'] = $photo_profile;
 	}
 	public function decode(array &$request, array $base = NULL)
     {
@@ -16,10 +17,17 @@ class ProvidersV2 extends Providers {
         {
             $id_integrated = $request['id_integrated'];
         }
+        if ( ! empty($request['photo_profile']))
+        {
+            $photo_profile = $request['photo_profile'];
+        }
         parent::decode($request);
             
         if(isset($id_integrated)) {
         $request['id_integrated'] = $id_integrated;
+        }
+        if(isset($photo_profile)) {
+            $request['photo_profile'] = $photo_profile;
         }
 	}
 }
