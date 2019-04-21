62c53294ca67df40ccee5290dc39ffe95d9d77e1
unknown <suongpham53@gmail.com>
2019-04-21 11:23:17 +0700

    add new function to update only provider id_integrated, fix get all provider

--- src/application/config/routes.php
+++ src/application/config/routes.php
@@ -96,5 +96,6 @@ $route['api/v2/customers/(:any)']['put'] = 'api/v2/customersv2/updateCustomer/$1
 $route['api/v2/providers/(:any)']['put'] = 'api/v2/providersv2/updateProvider/$1';
 $route['api/v2/providers/(:any)/(:any)']['put'] = 'api/v2/providersv2/updateProviderByServiceId/$1/$2';
 $route['api/v2/services/(:any)']['put'] = 'api/v2/servicesv2/updateService/$1';
+$route['api/v2/providers/update/(:any)/(:any)']['put'] = 'api/v2/providersv2/updateProviderIdIntegrated/$1/$2';
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
