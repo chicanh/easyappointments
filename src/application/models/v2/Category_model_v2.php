86a9a1c1a1139378e943c33751aca5038a7a4f3d
unknown <suongpham53@gmail.com>
2019-07-04 21:15:04 +0700

    update exception

--- src/application/models/v2/Category_model_v2.php
+++ src/application/models/v2/Category_model_v2.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
+use \EA\Engine\Api\V2\DuplicateException;
 class Category_model_v2 extends CI_Model {
 
     public function get($id) {
@@ -77,7 +77,7 @@ class Category_model_v2 extends CI_Model {
         if ($this->exists($category) && ! isset($category['id']))
         {
             // $category['id'] = $this->find_record_id($category);
-            throw new Exception('Duplicate record in database');
+            throw new DuplicateException('Duplicate record in database');
         }
 
         if ( ! isset($category['id']))
