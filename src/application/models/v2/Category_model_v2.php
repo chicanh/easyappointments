14da8e4c726b116848929684af54bd988ae2212b
unknown <suongpham53@gmail.com>
2019-07-16 14:43:28 +0700

    api to add spec to service

--- src/application/models/v2/Category_model_v2.php
+++ src/application/models/v2/Category_model_v2.php
@@ -143,7 +143,16 @@ class Category_model_v2 extends CI_Model {
     public function getCategoryId(array $id_integrateds) {
         return $this->db->select('id, id_integrated')->from('integrated_categories')
         ->where_in('id_integrated', $id_integrateds)->get()->result_array();
-        
+    }
+
+    public function getCategoryIdById_Integrated(array $id_integrateds) {
+        $categoryIds = $this->db->select('id')->from('integrated_categories')
+        ->where_in('id_integrated', $id_integrateds)->get()->result_array();
+        $ids = [];
+        foreach ($categoryIds as $entry) {
+            array_push($ids, $entry['id']);
+        }
+        return $ids;
     }
 }
 
