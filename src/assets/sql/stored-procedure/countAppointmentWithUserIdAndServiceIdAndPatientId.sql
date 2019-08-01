62fab3226fa2663557dd3b1339af01a775aa6237
Long Nguyen <long.ngu@ascendcorp.com>
2019-08-01 18:59:02 +0700

    update code

--- src/assets/sql/stored-procedure/countAppointmentWithUserIdAndServiceIdAndPatientId.sql
+++ src/assets/sql/stored-procedure/countAppointmentWithUserIdAndServiceIdAndPatientId.sql
@@ -6,36 +6,21 @@ CREATE PROCEDURE `countAppointmentsByCondition`(IN `idUserIntegrated` VARCHAR(20
                                                                            IN `startDate` VARCHAR(200), 
                                                                            IN `endDate` VARCHAR(200))
 BEGIN
-	Declare startOffset Integer;
-    #-- Prepare pre-condition before running main query
-    IF idServiceIntegrated <> '' THEN
-        SET idServiceIntegrated = (SELECT ea_services.id from ea_services WHERE ea_services.id_integrated = idServiceIntegrated);
-    END IF;
-
-    IF idUserIntegrated <> '' THEN
-        SET idUserIntegrated = (SELECT ea_users.id from ea_users WHERE ea_users.id_integrated = idUserIntegrated);
-    END IF;
-
-    IF idPatientIntegrated <> '' THEN
-        SET idPatientIntegrated = (SELECT ea_users.id from ea_users WHERE ea_users.id_integrated = idPatientIntegrated);
-    END IF;
-
-
     # -- Start build final Query with parameter
     SET @finalQuery = 'SELECT COUNT(ea_appointments.id) as "total" FROM ea_appointments WHERE 1 = 1';
-    IF idServiceIntegrated <> '' THEN
-    	SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.id_services = "',idServiceIntegrated,'"');
+    IF idServiceIntegrated <> '' OR idServiceIntegrated IS NOT NULL THEN
+    	SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.id_services = (SELECT id from ea_services WHERE id_integrated = "',idServiceIntegrated,'")');
     END IF;
-    IF idUserIntegrated <> '' THEN
-        SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.id_users_provider = "',idUserIntegrated,'"');
+    IF idUserIntegrated <> '' OR idUserIntegrated IS NOT NULL THEN
+        SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.id_users_provider = (SELECT id from ea_users WHERE id_integrated = "',idUserIntegrated,'")');
     END IF;
-    IF idPatientIntegrated <> '' THEN
-        SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.id_users_customer = "',idPatientIntegrated,'"');
+    IF idPatientIntegrated <> '' OR idPatientIntegrated IS NOT NULL THEN
+        SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.id_users_customer = (SELECT id from ea_users WHERE id_integrated = "',idPatientIntegrated,'")');
     END IF;
-    IF startDate <> '' THEN
+    IF startDate <> '' OR startDate IS NOT NULL THEN
     	SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.start_datetime >= "',startDate,'"');
 	END IF;
-    IF endDate <> '' THEN
+    IF endDate <> '' OR endDate IS NOT NULL THEN
         SET @finalQuery = CONCAT(@finalQuery,' AND ea_appointments.end_datetime <= "',endDate,'"');
     END IF;
     SET @finalQuery = CONCAT(@finalQuery, '  ORDER BY ea_appointments.id');
