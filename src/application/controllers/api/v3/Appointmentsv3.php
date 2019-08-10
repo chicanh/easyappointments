1509a38418234f75b11efdd0d94dfdfc9fcf8ab9
Long Nguyen <long.ngu@ascendcorp.com>
2019-08-10 09:40:17 +0700

    update code

--- src/application/controllers/api/v3/Appointmentsv3.php
+++ src/application/controllers/api/v3/Appointmentsv3.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /* ----------------------------------------------------------------------------
  * Easy!Appointments - Open Source Web Scheduler
@@ -14,9 +14,6 @@
 require_once __DIR__ . '/../v2/Appointmentsv2.php';
 
 use \EA\Engine\Api\V1\Response;
-use \EA\Engine\Api\V1\Request;
-use \EA\Engine\Types\NonEmptyText;
-
 
 /**
  * Appointments Controller v3
@@ -24,7 +21,8 @@ use \EA\Engine\Types\NonEmptyText;
  * @package Controllers
  * @subpackage API
  */
-class AppointmentsV3 extends AppointmentsV2 {
+class AppointmentsV3 extends AppointmentsV2
+{
 
     /**
      * Class Constructor
@@ -37,47 +35,51 @@ class AppointmentsV3 extends AppointmentsV2 {
         $this->parser = new \EA\Engine\Api\V2\Parsers\AppointmentsV2;
         $this->attachments_parser = new \EA\Engine\Api\V2\Parsers\AttachmentsV2;
     }
-    
-    public function get() {
+
+    public function get()
+    {
         try {
             $idUserIntegrated = $this->input->get('id_user_integrated');
             $idPatientIntegrated = $this->input->get('id_patient_integrated');
             $idServiceIntegrated = $this->input->get('id_service_integrated');
 
-            if($idUserIntegrated == null || $idServiceIntegrated == null){
+            if ($idUserIntegrated == null || $idServiceIntegrated == null) {
                 throw new \EA\Engine\Api\V1\Exception('id_user_integrated & id_service_integrated are required', 400);
             }
-          
-            $resultSet =  $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
+
+            $resultSet = $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
             $response = new Response($resultSet);
-            $response->output(); 
-            
+            $response->output();
+
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    public function getAppointmentWithServiceAndUserAndPatient($idServiceIntegrated, $idUserIntegrated, $idPatientIntegrated) {
+    public function getAppointmentWithServiceAndUserAndPatient($idServiceIntegrated, $idUserIntegrated, $idPatientIntegrated)
+    {
         try {
-            $resultSet =  $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
+            $resultSet = $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
             $response = new Response($resultSet);
-            $response->output(); 
+            $response->output();
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    public function getAppointmentWithServiceIdAndPatientId($idServiceIntegrated, $idPatientIntegrated) {
+    public function getAppointmentWithServiceIdAndPatientId($idServiceIntegrated, $idPatientIntegrated)
+    {
         try {
-            $resultSet =  $this->getListAppointmentsByConditions(null, $idServiceIntegrated, $idPatientIntegrated);
+            $resultSet = $this->getListAppointmentsByConditions(null, $idServiceIntegrated, $idPatientIntegrated);
             $response = new Response($resultSet);
-            $response->output(); 
+            $response->output();
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    private function getListAppointmentsByConditions($id_user_integrated, $id_service_integrated, $id_patient_integrated){
+    private function getListAppointmentsByConditions($id_user_integrated, $id_service_integrated, $id_patient_integrated)
+    {
         $page = $this->input->get('page');
         $size = $this->input->get('size');
         $sort = $this->input->get('sort');
@@ -95,9 +97,10 @@ class AppointmentsV3 extends AppointmentsV2 {
         return $resultSet;
     }
 
-    public function getUserAppointments($id_integrated) {
+    public function getUserAppointments($id_integrated)
+    {
         try {
-            if($this->input->get('id_user_integrated') != null) {
+            if ($this->input->get('id_user_integrated') != null) {
                 $appointments = $this->appointments_model_v3->getUserAppointments($id_integrated, $this->input->get('id_user_integrated'));
                 $response = new Response($appointments);
                 $response
@@ -106,62 +109,41 @@ class AppointmentsV3 extends AppointmentsV2 {
                     ->paginate()
                     ->minimize()
                     ->output();
-            }
-            else {
+            } else {
                 throw new \EA\Engine\Api\V1\Exception('id_user_integrated is required', 400);
             }
-            
+
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    private function encodedAppointments($appointments){
-        $encodedAppointments = [];  
-        foreach ($appointments as &$value){
-            array_push($encodedAppointments,$this->parser->customEncode($value));
+    private function encodedAppointments($appointments)
+    {
+        $encodedAppointments = [];
+        foreach ($appointments as &$value) {
+            array_push($encodedAppointments, $this->parser->customEncode($value));
         }
         return $encodedAppointments;
     }
 
-    public function getAppointmentsAddressStatistic($idServiceIntegrated){
-        try{
-            
-            $cities = $this->input->get('cities');
-            $startDate= $this->input->get('startDate');
-            $endDate = $this->input->get('endDate');
-            $gender =  $this->input->get('gender');
-            $idProviderIntegrated = $this->input->get('id_provider_integrated');
-            $firstTime = $this->checkIsValidBooleanType($this->input->get('firstTime')) ? $this->input->get('firstTime') : null;
-            $bhyt = $this->checkIsValidBooleanType($this->input->get('bhyt')) ? $this->input->get('bhyt') : null;
-
-            if(!trim($startDate)){
-                $startDate = date("Y-m").'-01';
-            }
-
-            if(!trim($endDate)){
-                $endDate = date("Y-m-t");
-            }
-
-            $endDate .= ' 23:59:59';
-
-            if(trim($gender) && $gender !== 'male' && $gender !== 'female'){
-                throw new \EA\Engine\Api\V1\Exception('Gender must be male or female', 400); 
-            }
-            if($cities !== null && $cities !== '' && !$this->containsOnlyNumber($cities)) {
-                throw new \EA\Engine\Api\V1\Exception('cities must contains only numbers', 400); 
-            }
-            if($this->getYearFromDateString($startDate) < 1905){
-                throw new \EA\Engine\Api\V1\Exception('startDate must greater than 1905', 400); 
-            }
-            if($this->getYearFromDateString($endDate) > date("Y")){
-                throw new \EA\Engine\Api\V1\Exception('endDate must less than current year', 400); 
-            }
+    public function getAppointmentsAddressStatistic($idServiceIntegrated)
+    {
+        try {
+            $requestParams = $this->validateInputRequestParams();
             $result = [];
-            foreach ($cities as $cityId) {
-                $element['id']= $cityId;
-                $element['city']= $this->cities_model->findCityBy($cityId)[0]['name'];
-                $element['data'] = $this->appointments_model_v3->getAddressBookingStatistic($idServiceIntegrated, $cityId, $startDate, $endDate, $gender, $firstTime, $bhyt, $idProviderIntegrated);
+            foreach ($requestParams['cities'] as $cityId) {
+                $element['id'] = $cityId;
+                $element['city'] = $this->cities_model->findCityBy($cityId)[0]['name'];
+                $element['data'] = $this->appointments_model_v3
+                    ->getAddressBookingStatistic($idServiceIntegrated,
+                                                $requestParams['cities'],
+                                                $requestParams['startDate'],
+                                                $requestParams['endDate'],
+                                                $requestParams['gender'],
+                                                $requestParams['firstTime'],
+                                                $requestParams['bhyt'],
+                                                $requestParams['id_provider_integrated']);
                 array_push($result, $element);
             }
             $response = new Response($result);
@@ -169,28 +151,72 @@ class AppointmentsV3 extends AppointmentsV2 {
         } catch (\Exception $exception) {
             exit($this->_handleException($exception));
         }
-
-
     }
 
-    private function checkIsValidBooleanType($value){
+    private function validateInputRequestParams()
+    {
+        $cities = $this->input->get('cities');
+        $startDate = $this->input->get('startDate');
+        $endDate = $this->input->get('endDate');
+        $gender = $this->input->get('gender');
+        $idProviderIntegrated = $this->input->get('id_provider_integrated');
+        $firstTime = $this->checkIsValidBooleanType($this->input->get('firstTime')) ? $this->input->get('firstTime') : null;
+        $bhyt = $this->checkIsValidBooleanType($this->input->get('bhyt')) ? $this->input->get('bhyt') : null;
+        if (!trim($startDate)) {
+            $startDate = date("Y-m") . '-01';
+        }
+
+        if (!trim($endDate)) {
+            $endDate = date("Y-m-t");
+        }
+
+        $endDate .= ' 23:59:59';
+        if (trim($gender) && $gender !== 'male' && $gender !== 'female') {
+            throw new \EA\Engine\Api\V1\Exception('Gender must be male or female', 400);
+        }
+        if ($cities !== null && $cities !== '' && !$this->containsOnlyNumber($cities)) {
+            throw new \EA\Engine\Api\V1\Exception('cities must contains only numbers', 400);
+        }
+        if ($this->getYearFromDateString($startDate) < 1905) {
+            throw new \EA\Engine\Api\V1\Exception('startDate must greater than 1905', 400);
+        }
+        if ($this->getYearFromDateString($endDate) > date("Y")) {
+            throw new \EA\Engine\Api\V1\Exception('endDate must less than current year', 400);
+        }
+
+        return ["cities" => $cities,
+            "startDate" => $startDate,
+            "endDate" => $endDate,
+            "gender" => $gender,
+            "id_provider_integrated" => $id_provider_integrated,
+            "firstTime" => $firstTime,
+            "bhyt" => $bhyt];
+    }
+
+    private function checkIsValidBooleanType($value)
+    {
         return $value !== null && $value !== '' && $value === 'TRUE' || $value == 'FALSE';
     }
 
-
-    private function getYearFromDateString($dateInString){
+    private function getYearFromDateString($dateInString)
+    {
         return intval(explode("-", $dateInString)[0]);
     }
 
-
-    private function containsOnlyNumber($array) {
+    private function containsOnlyNumber($array)
+    {
         $allNumeric = true;
-        foreach($array as $value) {
-             if (!(is_numeric($value))) {
-                  return false;
-             } 
+        foreach ($array as $value) {
+            if (!(is_numeric($value))) {
+                return false;
+            }
         }
         return true;
-   }
-    
+    }
+
+    public function getAppointmentsGenderStatistic($idServiceIntegrated)
+    {
+
+    }
+
 }
