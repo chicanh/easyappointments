243fabcee9b068fbe5df2a8b43f2b9385cdb958e
ntlong1594 <18412613+ntlong1594@users.noreply.github.com>
2019-08-16 10:06:04 +0700

    YOUMED-662 (#91)
    * update code
    
    * update code
    
    * update  code

--- src/application/models/v2/Providers_model_v2.php
+++ src/application/models/v2/Providers_model_v2.php
@@ -743,4 +743,22 @@ class Providers_Model_V2 extends CI_Model {
         ->join('integrated_provider_categories', 'integrated_provider_categories.id_categories = integrated_categories.id', 'inner')
         ->where('integrated_provider_categories.id_providers', $provider_id)->get()->result_array();;
     }
+
+    public function getProviderBy($name, $id_service_integrated){
+        $this->db->select('ea_users.*')->from('ea_users')
+                ->join('ea_services_providers', 'ea_services_providers.id_users = ea_users.id','inner')
+                ->join('ea_services', 'ea_services_providers.id_services = ea_services.id','inner')
+                ->where('ea_users.id_roles = 2');
+        if ($name){
+            $this->db->where("CONCAT(ea_users.first_name, ' ', ea_users.last_name) = '".$name."'");
+        }
+        if($id_service_integrated){
+            $this->db->where('ea_services.id_integrated', $id_service_integrated);
+        }
+
+        $result = $this->db->get()->result_array();
+        return $result;
+    }
+
+
 }
