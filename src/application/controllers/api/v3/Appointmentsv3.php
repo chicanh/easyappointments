59f4ad179ab694a04f2e1d3018f5cabb5be2dc9d
ntlong1594 <18412613+ntlong1594@users.noreply.github.com>
2019-08-16 10:00:28 +0700

    Youmed 658 (#89)
    * add stored procedure
    
    * update code
    
    * update code
    
    * update code
    
    * update code
    
    * update
    
    * u
    
    * update code
    
    * update code
    
    * update code
    
    * update code
    
    * update code
    
    * update code

--- src/application/controllers/api/v3/Appointmentsv3.php
+++ src/application/controllers/api/v3/Appointmentsv3.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /* ----------------------------------------------------------------------------
  * Easy!Appointments - Open Source Web Scheduler
@@ -14,8 +14,6 @@
 require_once __DIR__ . '/../v2/Appointmentsv2.php';
 
 use \EA\Engine\Api\V1\Response;
-use \EA\Engine\Api\V1\Request;
-use \EA\Engine\Types\NonEmptyText;
 
 /**
  * Appointments Controller v3
@@ -23,7 +21,8 @@ use \EA\Engine\Types\NonEmptyText;
  * @package Controllers
  * @subpackage API
  */
-class AppointmentsV3 extends AppointmentsV2 {
+class AppointmentsV3 extends AppointmentsV2
+{
 
     /**
      * Class Constructor
@@ -32,50 +31,55 @@ class AppointmentsV3 extends AppointmentsV2 {
     {
         parent::__construct();
         $this->load->model('/v3/appointments_model_v3');
+        $this->load->model('cities_model');
         $this->parser = new \EA\Engine\Api\V2\Parsers\AppointmentsV2;
         $this->attachments_parser = new \EA\Engine\Api\V2\Parsers\AttachmentsV2;
     }
-    
-    public function get() {
+
+    public function get()
+    {
         try {
             $idUserIntegrated = $this->input->get('id_user_integrated');
             $idPatientIntegrated = $this->input->get('id_patient_integrated');
             $idServiceIntegrated = $this->input->get('id_service_integrated');
 
-            if($idUserIntegrated == null || $idServiceIntegrated == null){
+            if ($idUserIntegrated == null || $idServiceIntegrated == null) {
                 throw new \EA\Engine\Api\V1\Exception('id_user_integrated & id_service_integrated are required', 400);
             }
-          
-            $resultSet =  $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
+
+            $resultSet = $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
             $response = new Response($resultSet);
-            $response->output(); 
-            
+            $response->output();
+
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    public function getAppointmentWithServiceAndUserAndPatient($idServiceIntegrated, $idUserIntegrated, $idPatientIntegrated) {
+    public function getAppointmentWithServiceAndUserAndPatient($idServiceIntegrated, $idUserIntegrated, $idPatientIntegrated)
+    {
         try {
-            $resultSet =  $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
+            $resultSet = $this->getListAppointmentsByConditions($idUserIntegrated, $idServiceIntegrated, $idPatientIntegrated);
             $response = new Response($resultSet);
-            $response->output(); 
+            $response->output();
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    public function getAppointmentWithServiceIdAndPatientId($idServiceIntegrated, $idPatientIntegrated) {
+    public function getAppointmentWithServiceIdAndPatientId($idServiceIntegrated, $idPatientIntegrated)
+    {
         try {
-            $resultSet =  $this->getListAppointmentsByConditions(null, $idServiceIntegrated, $idPatientIntegrated);
+            $resultSet = $this->getListAppointmentsByConditions(null, $idServiceIntegrated, $idPatientIntegrated);
             $response = new Response($resultSet);
-            $response->output(); 
+            $response->output();
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    private function getListAppointmentsByConditions($id_user_integrated, $id_service_integrated, $id_patient_integrated){
+    private function getListAppointmentsByConditions($id_user_integrated, $id_service_integrated, $id_patient_integrated)
+    {
         $page = $this->input->get('page');
         $size = $this->input->get('size');
         $sort = $this->input->get('sort');
@@ -93,9 +97,10 @@ class AppointmentsV3 extends AppointmentsV2 {
         return $resultSet;
     }
 
-    public function getUserAppointments($id_integrated) {
+    public function getUserAppointments($id_integrated)
+    {
         try {
-            if($this->input->get('id_user_integrated') != null) {
+            if ($this->input->get('id_user_integrated') != null) {
                 $appointments = $this->appointments_model_v3->getUserAppointments($id_integrated, $this->input->get('id_user_integrated'));
                 $response = new Response($appointments);
                 $response
@@ -104,23 +109,47 @@ class AppointmentsV3 extends AppointmentsV2 {
                     ->paginate()
                     ->minimize()
                     ->output();
-            }
-            else {
+            } else {
                 throw new \EA\Engine\Api\V1\Exception('id_user_integrated is required', 400);
             }
-            
+
         } catch (\Exception $exception) {
-                    exit($this->_handleException($exception));
+            exit($this->_handleException($exception));
         }
     }
 
-    private function encodedAppointments($appointments){
-        $encodedAppointments = [];  
-        foreach ($appointments as &$value){
-            array_push($encodedAppointments,$this->parser->customEncode($value));
+    private function encodedAppointments($appointments)
+    {
+        $encodedAppointments = [];
+        foreach ($appointments as &$value) {
+            array_push($encodedAppointments, $this->parser->customEncode($value));
         }
         return $encodedAppointments;
     }
 
-    
+    public function getAppointmentsDistrictStatistic($idServiceIntegrated)
+    {
+        $this->load->helper('validation_helper');
+        try {
+            $requestParams = validateInputRequestParamsForStatictis($this->input);
+            $result = $this->appointments_model_v3->getAddressBookingStatistic($idServiceIntegrated,
+                                                $requestParams['cities'],
+                                                $requestParams['startDate'],
+                                                $requestParams['endDate'],
+                                                $requestParams['gender'],
+                                                $requestParams['firstTime'],
+                                                $requestParams['useHealthInsurance'],
+                                                $requestParams['id_provider_integrated']);
+            $response = new Response($result);
+            $response->output();
+        } catch (\Exception $exception) {
+            exit($this->_handleException($exception));
+        }
+    }
+
+    public function getAppointmentsGenderStatistic($idServiceIntegrated)
+    {
+
+    }
+
 }
---
+++ src/assets/sql/stored-procedure/getDistrictsBookingStatisticByConditions.sql
@@ -0,0 +1,47 @@
+DROP PROCEDURE IF EXISTS `getAddressBookingStatisticByConditions`;
+DROP PROCEDURE IF EXISTS `getDistrictsBookingStatisticByConditions`;
+CREATE PROCEDURE `getDistrictsBookingStatisticByConditions`(IN `idServiceIntegrated` VARCHAR(200), IN `cityId` INT(11), IN `startDate` VARCHAR(200), IN `endDate` VARCHAR(200), IN `gender` VARCHAR(200), IN `firstTimeBooking` VARCHAR(200), IN `healthInsuranceUsed` VARCHAR(200), IN `idProviderIntegrated` VARCHAR(200))
+BEGIN
+    SET @finalQuery = CONCAT('SELECT integrated_cities.name as city,integrated_districts.name as district, COUNT(integrated_districts.name) as value  ',
+                      'FROM ea_users eau ', 
+                      'INNER JOIN integrated_users_patients iup ON eau.id = iup.id_patients ',
+                      'INNER JOIN integrated_cities  ON eau.city_id = integrated_cities.id ',
+                      'INNER JOIN integrated_districts  ON integrated_districts.id_city = eau.city_id AND integrated_districts.id = eau.district_id ',
+                      'INNER JOIN ea_appointments eaa ON eaa.id_users_customer = eau.id ');
+    
+    IF idProviderIntegrated IS NOT NULL AND idProviderIntegrated <> '' THEN
+        SET @finalQuery = CONCAT(@finalQuery, 'AND eaa.id_users_provider IN (SELECT id from ea_users WHERE find_in_set(id_integrated,"',idProviderIntegrated,'")) ');
+    END IF;
+
+    
+    SET @finalQuery = CONCAT(@finalQuery,'WHERE iup.id_service_integrated = "',idServiceIntegrated, '" '
+                      'AND eaa.start_datetime >= "',startDate,'" ',
+                      'AND eaa.end_datetime <= "',endDate, '"');
+                      
+    IF cityId IS NOT NULL THEN
+        SET @finalQuery = CONCAT(@finalQuery, 'AND find_in_set(eau.city_id,', cityId, ') ');
+    END IF;
+
+    IF gender IS NOT NULL THEN
+        SET @finalQuery = CONCAT(@finalQuery, 'AND eau.gender = "',gender,'" ');
+    END IF;
+
+    IF healthInsuranceUsed IS NOT NULL AND upper(healthInsuranceUsed) = 'TRUE' THEN
+        SET @finalQuery = CONCAT(@finalQuery, 'AND eaa.health_insurance_used = TRUE ');
+    END IF;
+
+    IF healthInsuranceUsed IS NOT NULL AND upper(healthInsuranceUsed) = 'FALSE' THEN
+        SET @finalQuery = CONCAT(@finalQuery, 'AND eaa.health_insurance_used = FALSE ');
+    END IF;
+
+    IF firstTimeBooking IS NOT NULL AND upper(firstTimeBooking) = 'TRUE' THEN
+        SET @finalQuery = CONCAT(@finalQuery, 'AND iup.first_booking_date >= "', startDate, '" AND iup.first_booking_date <= "',endDate,'" ');
+    END IF; 
+
+    SET @finalQuery = CONCAT(@finalQuery, 'GROUP BY integrated_districts.name;');
+    #SELECT @finalQuery;
+
+    PREPARE stmt FROM @finalQuery;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END;
